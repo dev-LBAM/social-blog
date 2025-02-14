e19daf73da423a900c0bd5e31c56edf9754db5e4
dev-LBAM <lucasbatist.a@hotmail.com>
2025-02-14 08:02:47 -0300

    Add update-user method
    Criei o sistema para atualizar os dados do usuario, recebendo ao menos 1 dado para ser modificado

---
+++ src/app/api/user/(controllers)/update-user/[id]/route.ts
@@ -0,0 +1,56 @@
+import { updateUserService } from '../../../(services)/update-user.service'
+import { updateUserDTO } from '../../../(dtos)/update-user.dto'
+import { NextRequest, NextResponse } from 'next/server'
+import { z } from 'zod'
+
+export async function PUT(req: NextRequest) 
+{
+  try 
+  {
+    const userId = req.nextUrl.pathname.split('/')[4]
+
+    const body = await req.json()
+
+    const validatedData = updateUserDTO.parse(body)
+
+    if (Object.keys(validatedData).length === 0) 
+    {
+      return NextResponse.json(
+        { message: 'At least one field must be provided for update.' }, 
+        { status: 400 })
+    }
+
+    const response = await updateUserService(userId, validatedData)
+
+    if (!response) 
+    {
+      return NextResponse.json(
+          { message: 'User not found' },
+          { status: 404 }
+      )
+    }
+    else
+    {
+      return NextResponse.json(
+        { message: 'User updated successfully', response }, 
+        { status: 200 })
+    }
+  } 
+    catch (error) 
+    {
+        if (error instanceof z.ZodError)
+        {
+            return NextResponse.json(
+                {message: 'Validation error', details: error.errors}, 
+                {status: 400})                 
+        } 
+        else 
+        {
+            console.error('\u{274C} Internal server error while updating user: ', error)
+            return NextResponse.json(
+                {message: 'Internal server error, please try again later'},
+                {status: 500})
+        }  
+    }
+}
+
---
+++ src/app/api/user/(dtos)/update-user.dto.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod'
+
+const passwordSchema = z
+    .string()
+    .min(8, 'Password must be at least 8 characters long.')
+    .max(20, 'Password must be at most 20 characters long.')
+    .regex(/[a-z]/, 'Password must contain at least one lowercase letter.')
+    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter.')
+    .regex(/\d/, 'Password must contain at least one number.')
+    .regex(/[@$!%*?&]/, 'Password must contain at least one special character (@, $, !, %, *, ?, &).')
+
+const phoneSchema = z
+.string()
+.regex(/^(\+55)?\s?(?:\(?[1-9][0-9]\)?)\s?(?:9[1-9][0-9]{3}-?[0-9]{4})$/, 'Invalid phone number format. Example: +55 11 91234-5678')
+
+export const updateUserDTO = z.object(
+{
+    name: z.string().min(1, 'Name is required.'),
+    email: z.string().email('Invalid email address.'),
+    password: passwordSchema,
+    phone: phoneSchema,
+    age: z.number().min(13, 'Minimum age is 13 years old.').max(130),
+    birthDate: z.date(),
+    city: z.string().min(1, 'City is required.'),
+    country: z.string().min(1, 'Country is required.')
+}).partial()
+
+export type UpdateUserDTO = z.infer<typeof updateUserDTO>
