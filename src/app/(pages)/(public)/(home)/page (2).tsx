c1b293ea5e363d2ecd5ebc1efcff4d4b600dcf84
dev-LBAM <lucasbatist.a@hotmail.com>
2025-03-02 17:07:59 -0300

    commit fix

--- src/app/(pages)/(public)/(home)/page (2).tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
-        <h1 className="text-4xl font-bold">Tailwind está funcionando! 🚀</h1>
-      </main>
-    );
-  }
-  
