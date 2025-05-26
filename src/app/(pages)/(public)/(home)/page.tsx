131f540dbdbbeab2e87e972b51b5e67a685daab6
dev-LBAM <lucasbatist.a@hotmail.com>
2025-05-26 20:16:01 -0300

    Update page.tsx

--- src/app/(pages)/(public)/(home)/page.tsx
+++ src/app/(pages)/(public)/(home)/page.tsx
@@ -15,19 +15,21 @@ export default function HomePage()
     <div className="flex items-center justify-center h-screen bg-page">
 
       <div className="max-w-md w-full p-8 rounded-sm bg-box drop-shadow-lg" >
-        
+        <a className="text-color text-sm drop-shadow-md underline absolute right-2 top-2 cursor-pointer" href="/feed">Explore Without Account→</a>
         {isRegister ? <CheckEmail /> : <LoginForm />}
         <div className="flex items-center gap-2 text-input-title pt-8 rounded-xl max-w-md w-full">
           <p>{isRegister ? "Already have an account?" : "Don't have an account?"}</p>
           <button
             onClick={() => setIsRegister(!isRegister)}
-            className="text-color  drop-shadow-md underline transition-transform duration-200 ease-in-out hover:scale-105 cursor-pointer "
+            className="text-color drop-shadow-md underline transition-transform duration-200 ease-in-out hover:scale-105 cursor-pointer "
           >
             {isRegister ? "Log In" : "Sign Up"}
           </button>
+          
         </div>
-      </div>
 
+      </div>
+    
     </div>
   )
 }
